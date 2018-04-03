c1bf477b6528f39310307f0301ead67f518ed8ea
Kimmy Hsu <KIMMY@kimmys-macbook-pro.local>
2018-04-03 21:27:54 +0800

    Initial Commit

---
+++ HW_創作自己有興趣的app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW_創作自己有興趣的app
+//
+//  Created by Kimmy Hsu on 03/04/2018.
+//  Copyright © 2018 Kimmy Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
